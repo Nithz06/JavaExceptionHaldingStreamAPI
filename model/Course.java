49619d8bc9afb166749c8450ea360e57951d12ab
Nithz06 <122868193+Nithz06@users.noreply.github.com>
2024-06-13 14:06:15 +0700

    fisrt commit

---
+++ model/Course.java
@@ -0,0 +1,16 @@
+package model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.time.LocalDate;
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Course {
+    private int id;
+    private String title;
+    private String [] instructorNames;
+    private String [] requirements;
+    private LocalDate startDate;
+}
